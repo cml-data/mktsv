22235d4b08c27260f1d0111fb2248170d0fa6897
cml-data <157057361+cml-data@users.noreply.github.com>
2024-06-14 19:25:50 -0700

    Add files via upload

---
+++ make_tsv.py
@@ -0,0 +1,16 @@
+import hashlib, base64, requests
+from github import Github
+
+PREFIX = 'https://raw.githubusercontent.com/cd-public/books/main/' # DONT scrape Gutenberg
+BK_DIR = '../books/'
+
+with open('books.tsv', 'w') as f:
+    f.write('TsvHttpData-1.0\n')
+    g = Github()
+    repo = g.get_repo("cd-public/books")
+    for file in repo.get_contents(""):
+        contents = requests.get(PREFIX + file.name)
+        content = contents.content
+        base64hash = base64.b64encode(hashlib.md5(content).digest())
+        f.write(PREFIX + file.name + "\t" + str(len(content)) + "\t" + base64hash.decode('utf-8') + "\n")
+    f.close()
